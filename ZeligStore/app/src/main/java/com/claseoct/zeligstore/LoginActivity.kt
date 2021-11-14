c906a3f953d4b364f1bd27a057b83fa23c9b1283
Mauri20 <69651999+Mauri20@users.noreply.github.com>
2021-11-13 23:55:13 -0600

    Login y Registro de usuario
    Se puede iniciar sesion con 5 usuarios estaticos, o tambien se pueden agregar otros que sirvan durante la ejecucion de la aplicacion

--- ZeligStore/app/src/main/java/com/claseoct/zeligstore/LoginActivity.kt
+++ ZeligStore/app/src/main/java/com/claseoct/zeligstore/LoginActivity.kt
@@ -4,12 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import java.time.Duration
+import kotlin.system.exitProcess
 
 class LoginActivity : AppCompatActivity() {
+
+    //Preparando las variables
+    private lateinit var etUser:EditText
+    private lateinit var etPassword: EditText
+    //Datos de usuarios para el logueo
+    var userOne= arrayOf("Mauricio","Mauri","7227-2324","1234")
+    var userTwo= arrayOf("Fatima","Fati","7734-2445","1234")
+    var userThree= arrayOf("Andres","Andres","7275-6724","1234")
+    var userFour= arrayOf("Jasmin","Jas","7646-9824","1234")
+    var userFive= arrayOf("Joshua","Josh","7765-2904","1234")
+    //variable con los datos para inicio de sesion
+    var credentials= arrayListOf(userOne,userTwo,userThree,userFour,userFive)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //Asignando los componentes a las variables
+        etUser=findViewById(R.id.et_User)
+        etPassword=findViewById(R.id.et_Password)
+
+        //Recogiendo datos del Intent
+        val miIntent=intent.extras
+        val name=miIntent?.getString("name").toString()
+        val user=miIntent?.getString("user").toString()
+        val phone=miIntent?.getString("phone").toString()
+        val password=miIntent?.getString("pass").toString()
+        if(name!="null"&&user!="null"&&phone!="null"&&password!="null"){
+            val newUser= arrayOf(name,user,phone,password)
+            credentials.add(newUser)
+        }
+        credentials.forEach { println(it.get(1)) }
     }
+    //Funcion para evaluar si hay registro de credenciales
+    fun evalCredentials(view: View){
+        var result=false
+        var contador=0
+        credentials.forEach{
+            result = etUser.text.toString().equals(it.get(1)) && etPassword.text.toString().equals(it.get(3))
+            if (result) contador=1
+        }
+        if(contador==1){
+            startActivity(Intent(this,MenuActivity::class.java))
+            contador=0
+        }else {
+            Toast.makeText(this, "Credenciales Incorrectas", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //Funcion para ir al Registro
     fun onLoginClick(view: View?) {
         startActivity(Intent(this,NewUserActivity::class.java))
         overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
--- ZeligStore/app/src/main/java/com/claseoct/zeligstore/NewUserActivity.kt
+++ ZeligStore/app/src/main/java/com/claseoct/zeligstore/NewUserActivity.kt
@@ -4,12 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class NewUserActivity : AppCompatActivity() {
+    //Creando las variables para los objetos
+    private lateinit var etName:EditText
+    private lateinit var etUser:EditText
+    private lateinit var etPhone:EditText
+    private lateinit var etPassword:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_user)
+        //Asignando los objetos a las variables
+        etName=findViewById(R.id.et_Name)
+        etUser=findViewById(R.id.et_User)
+        etPhone=findViewById(R.id.et_Phone)
+        etPassword=findViewById(R.id.et_Password)
     }
+    //Funcion para guardar
+    fun saveUser(view: View?){
+        //variables contenedoras del texto de los campos
+        var name=etName.text.toString()
+        var user=etUser.text.toString()
+        var phone=etPhone.text.toString()
+        var password=etPassword.text.toString()
+        //Evaluando si estan vacias
+        if (!name.isEmpty()&&!user.isEmpty()&&!phone.isEmpty()&&!password.isEmpty()){
+            var newUser2= arrayOf(name,user,phone,password)
+            Toast.makeText(this,"Usuario Registrado",Toast.LENGTH_LONG).show()
+            val inten=Intent(this,LoginActivity::class.java)
+            inten.putExtra("name",name)
+            inten.putExtra("user",user)
+            inten.putExtra("phone",phone)
+            inten.putExtra("pass",password)
+            startActivity(inten)
+        }else{
+            Toast.makeText(this,"Debe completar los campos",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //Funcion para regresar a la vista anterior
     fun onLoginClick(view: View?) {
         startActivity(Intent(this,LoginActivity::class.java))
         overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
