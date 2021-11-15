6aa63138eb1e9f2d2d273156659be33967070f72
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2021-11-14 21:16:50 -0600

    New Changes and Adds

--- ZeligStore/app/src/main/java/com/claseoct/zeligstore/MenuActivity.kt
+++ ZeligStore/app/src/main/java/com/claseoct/zeligstore/MenuActivity.kt
@@ -1,13 +1,57 @@
 package com.claseoct.zeligstore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
 
+    //Funcion para seleccionar una marca
+    fun selectMarca(Vista: View){
+        when(Vista.id){
+            R.id.imgBtn_puma ->
+            {
+                Toast.makeText(this, "Has seleccionado PUMA", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,PumaActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+            R.id.imgBtn_adidas ->
+            {
+                Toast.makeText(this, "Has seleccionado ADIDAS", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,AdidasActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+            R.id.imgBtn_converse ->
+            {
+                Toast.makeText(this, "Has seleccionado CONVERSE", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,ConverseActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+            R.id.imgBtn_nike ->
+            {
+                Toast.makeText(this, "Has seleccionado NIKE", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,NikeActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+            R.id.imgBtn_reebok ->
+            {
+                Toast.makeText(this, "Has seleccionado REEBOK", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,ReebokActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+            R.id.imgBtn_vans ->
+            {
+                Toast.makeText(this, "Has seleccionado VANS", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,VansActivity::class.java))
+                overridePendingTransition(R.anim.slide_in_right,android.R.anim.slide_out_right);
+            }
+        }
     }
 }
---
+++ ZeligStore/app/src/main/java/com/claseoct/zeligstore/VansActivity.kt
@@ -0,0 +1,11 @@
+package com.claseoct.zeligstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VansActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vans)
+    }
+}
