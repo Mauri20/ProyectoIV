2e701e4c590cd1a713affd4fd044f571894520a9
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2021-11-17 16:56:27 -0600

    Creación de proyecto 'API ZeligStore'

---
+++ ZeligStoreOficial/src/main/java/com/zeligstoreoficial/ZeligStoreOficial/ZeligStoreOficialApplication.java
@@ -0,0 +1,13 @@
+package com.zeligstoreoficial.ZeligStoreOficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeligStoreOficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeligStoreOficialApplication.class, args);
+	}
+
+}
---
+++ ZeligStoreOficial/src/test/java/com/zeligstoreoficial/ZeligStoreOficial/ZeligStoreOficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeligstoreoficial.ZeligStoreOficial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeligStoreOficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
